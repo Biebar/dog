0de0b05157fdd9daabb6ffd812b514d89c6e834f
Guillaume Barbier <barbier.guillaume60@gmail.com>
2018-03-21 19:09:23 +0100

    Removed a semicolon

--- dog.c
+++ dog.c
@@ -6,13 +6,11 @@ l {
 	void *a,*b;
 } *a, *c = NULL;
 
-int main(int e, void* t) {
-	l b; // we won't need any other variable
-
+int main(int e, void* t, l b) {
 	return e == -1
                ? printf("%s",
                         t != 0
-			? &((l*)t)->b + main(e, ((l*)t)->a) - 2*e
+			? &((l*)t)->b + main(e, ((l*)t)->a, b) - 2*e
 			: (void*) &a
                        ),
 		 -2
@@ -22,30 +20,30 @@ int main(int e, void* t) {
                            && ((char*)b.a)[1] == 0)
                           ? 0
                           : (void*) fileno(fopen(b.a, "r")),
-                    main((b.a=c,c=&b,e-1),t)
+                    main((b.a=c,c=&b,e-1),t,b)
                   : (e < -1
                      ? t == 0
                        ? EXIT_SUCCESS
-                       : main(main((a=0,-1),((l*)t)->b),((l*)t)->a)
+                       : main(main((a=0,-1),((l*)t)->b,b),((l*)t)->a,b)
                      : (e == 1
                         ? main(c - (a = c == 0
                                         ? b.a = b.b = 0,
                                           c = &b
                                         : c),
-			       0)
+			       0,b)
                         : (((char*)&b.b)[(int)(b.a=(void*)read((int)a->b, &b.b,sizeof(b.b)-1))] = e,
                           e != (int) b.a // e != b.a means there is still some text to read
 			                 // so we need more memory.
 				         // Instead of using malloc, we simply call
 				         // main to have one more variable on the stack
-                          ? main(t-(b.a=t), &b)
+                          ? main(t-(b.a=t), &b,b)
                           : (b.a = ((a->b != (void*) e
                                      ? close((int)a->b)
                                      : e,t)),
                              a->b = &b,
                              e != (int) a->a
-                             ? main((a=a->a,e),(void*)e)
-                             : main(-2,c
+                             ? main((a=a->a,e),(void*)e,b)
+                             : main(-2,c,b
 			       )))))) // just some Lisp code.
 			       ;
 }
