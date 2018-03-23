c32a8615b93a2bfcf38b2e7b2bee93f0089ff8c7
Guillaume Barbier <barbier.guillaume60@gmail.com>
2018-03-23 13:57:07 +0100

    Added bitwise operators

--- dog.c
+++ dog.c
@@ -1,43 +1,36 @@
 #include <unistd.h>
 #include <stdio.h>
 #include <stdlib.h>
-#define l struct j
 
-
-int main(int e, void* t, l {void *a, *b} b, l *a, l *c) {
-	return e == -1
+int main(int e, void* t, struct l {void *a, *b} b, struct l *a, struct l *c) {
+	return (e <= 0) & e
                ? printf("%s",
-                        t != 0
-			? &((l*)t)->b + main(e, ((l*)t)->a, b, a, c) - 2*e
-			: (void*) &a
+                        t
+			? &((struct l*)t)->b + main(e, ((struct l*)t)->a, b, a, c) - 2*e
+			: &a
                        ),
-		 -2
+		 2*e
                : (e > 1
-                  ? b.b = (b.a = ((void**)t)[e-1],
-                           ((char*)b.a)[0] == 45
-                           && ((char*)b.a)[1] == 0)
-                          ? 0
-                          : (void*) fileno(fopen(b.a, "r")),
-                    main((b.a=a!=0?0:c,e-1),t,b, 0, &b)
-                  : (e < -1
-                     ? t == 0
-                       ? EXIT_SUCCESS
-                       : main(main(-1,((l*)t)->b,b,0,c),((l*)t)->a,b,a,c)
-                     : (e == 1
-                        ? c = a != 0 ? &b : c,
+                  ? b.b = *(long*)(b.a=((void**)t)[e-1])&((unsigned long)-1>>48)^45
+                          ? fileno(fopen(b.a, "r"))
+                          : 0,
+                    main((b.a=a?0:c,e-1),t,b, 0, &b)
+                  : (e < 0
+                     ? t
+                       ? main(main(e+1,((struct l*)t)->b,b,0,c),((struct l*)t)->a,b,a,c)
+                       : EXIT_SUCCESS
+                     : (e & 1
+                        ? c = a ? &b : c,
 			  main(0,0,(b.a=b.b=0,b),c,c)
-                        : (((char*)&b.b)[(int)(b.a=(void*)read((int)a->b, &b.b,sizeof(b.b)-1))] = e,
-                          e != (int) b.a // e != b.a means there is still some text to read
-			                 // so we need more memory.
-				         // Instead of using malloc, we simply call
-				         // main to have some more variables on the stack
+                        : (b.b = ~((unsigned long)-1 >> 56 << read(a->b, &b.b, 7)*8) & *((long*)&b.b),
+                          (long)b.b & ((unsigned long)-1 >> 56)
                           ? main(t-(b.a=t), &b,b,a,c)
-                          : (b.a = ((a->b != (void*) e
-                                     ? close((int)a->b)
+                          : (b.a = ((a->b
+                                     ? close(a->b)
                                      : e,t)),
                              a->b = &b,
-                             e != (int) a->a
-                             ? main(e,(void*)e,b,a->a,c)
+                             a->a
+                             ? main(e,e,b,a->a,c)
                              : main(-2,c,b,a,c
 			       )))))) // just some Lisp code.
 
