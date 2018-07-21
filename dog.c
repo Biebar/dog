e116c7d6a0ba0d07e988948edb0303d0aedf3ac3
Guillaume Barbier <barbier.guillaume60@gmail.com>
2018-07-22 00:08:09 +0200

    More K&R, Less ternary operators

--- dog.c
+++ dog.c
@@ -1,35 +1,37 @@
 #include <unistd.h>
 
-int main(int e, void* t, struct l {struct l *a, *b} b, struct l *a, struct l *c) {
-	return (e <= 0) & e
-               ? write(a,
-                       t ? &((struct l*)t)->b + main(e, ((struct l*)t)->a, b, a, c) - 2*e : t,
-		       t ? (long)((struct l*)t)->b >> 56 : a
-		      ),
-		 2*e
-               : (e > 1
-                  ? b.b = *(long*)(b.a=((void**)t)[e-1])&((unsigned long)-1>>48)^45
-                          ? open(b.a,0)
-                          : 0,
-                    main((b.a=a?0:c,e-1),t,b, 0, &b)
-                  : (e < 0
-                     ? t
-                       ? main(main(e+1,((struct l*)t)->b,b,0,c),((struct l*)t)->a,b,a,c)
-                       : 0
-                     : (e & 1
-                        ? c = a ? &b : c,
-			  main(0,0,(b.a=b.b=0,b),c,c)
-                        : (b.b = (long)read(a->b, &b.b, 7)<<56 | ((long)b.b & ((unsigned long)-1>>8)),
-			  (long)b.b>>56
-                          ? main(t-(void*)(b.a=t), &b,b,a,c)
-                          : (b.a = ((a->b
-                                     ? close(a->b)
-                                     : e,t)),
-                             a->b = &b,
-                             a->a
-                             ? main(e,e,b,a->a,c)
-                             : main(-2,c,b,a,c
-			       )))))) // just some Lisp code.
 
-        ; // The only semicolon in the entire program.
+int main(e,t,b,a)
+struct l {struct l *a, *b} b,a,*t;
+{
+	return e <= 0 & e
+               && (write(a.a,
+		       (t&&main(e,t->a,b,a))*(long)&t->b,
+		       (t&&((a.b=(long)t->b>>56),1))*((long)a.b-(long)a.a)+(long)a.a
+		      ),
+		 -e)
+               || (e > 1
+                  && !main((a.a&&((b.a=0),1)||(b.a=a.b),e-1),
+			    t,b,0,&b,
+			    (*(long*)(b.a=((void**)t)[e-1])&((unsigned long)-1>>48)^45)
+			    &&(b.b=open(b.a,0))||(b.b=0)
+			    )
+                  || (e < 0
+                     && ((t && main(main(e+1,t->b,b,0,a.b)-3,t->a,b,a)),1)
+                     || (e & 1
+                        && !main(0,b,0,a.b,a.b,a.a&&(a.b=&b),b.a=b.b=0)
+                        || (b.b = (long)read(a.a->b, &b.b, 7)<<56 | ((long)b.b & ((unsigned long)-1>>8)),
+			  (long)b.b>>56
+                          && !main(t-(b.a=t), &b,b,a)
+                          || (b.a = (a.a->b
+                                     && close(a.a->b)
+                                     ,t),
+                             a.a->b = &b,
+                             a.a->a
+                             && !main(e,e,b,a.a->a,a.b)
+                             || main(-2,a.b,b,a
+			       ))))),0)
+
+        ;
 }
+
