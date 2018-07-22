80b7e2e38dbcc4f025e0df8bf87455f7e8628d68
Guillaume Barbier <barbier.guillaume60@gmail.com>
2018-07-22 17:11:23 +0200

    cleanup

--- dog.c
+++ dog.c
@@ -1,15 +1,12 @@
 #include <unistd.h>
 
-
-int main(e,t,b,a)
+main(e,t,b,a)
 struct l {struct l *a, *b} b,a,*t;
 {
 	return e <= 0 & e
-               && (write(a.a,
+               && write(a.a,
 		       (t&&main(e,t->a,b,a))*(long)&t->b,
-		       (t&&((a.b=(long)t->b>>56),1))*((long)a.b-(long)a.a)+(long)a.a
-		      ),
-		 -e)
+		       (t&&((a.b=(long)t->b>>56),1))*((long)a.b-(long)a.a)+(long)a.a)+2
                || (e > 1
                   && !main((a.a&&((b.a=0),1)||(b.a=a.b),e-1),
 			    t,b,0,&b,
@@ -17,11 +14,10 @@ struct l {struct l *a, *b} b,a,*t;
 			    &&(b.b=open(b.a,0))||(b.b=0)
 			    )
                   || (e < 0
-                     && ((t && main(main(e+1,t->b,b,0,a.b)-3,t->a,b,a)),1)
+                     && !(t && main(main(e+1,t->b,b,0,a.b)-3,t->a,b,a))
                      || (e & 1
                         && !main(0,b,0,a.b,a.b,a.a&&(a.b=&b),b.a=b.b=0)
-                        || (b.b = (long)read(a.a->b, &b.b, 7)<<56 | ((long)b.b & ((unsigned long)-1>>8)),
-			  (long)b.b>>56
+                        || ((long)(b.b = (long)read(a.a->b, &b.b, 7)<<56 | ((long)b.b & ((unsigned long)-1>>8)))>>56
                           && !main(t-(b.a=t), &b,b,a)
                           || (b.a = (a.a->b
                                      && close(a.a->b)
@@ -29,8 +25,7 @@ struct l {struct l *a, *b} b,a,*t;
                              a.a->b = &b,
                              a.a->a
                              && !main(e,e,b,a.a->a,a.b)
-                             || main(-2,a.b,b,a
-			       ))))),0)
+                             || main(-2,a.b,b,a))))),0)
 
         ;
 }
